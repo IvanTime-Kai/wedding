32286d840897dcf64746fb99c4c1b516ba6d532d
Ivantime <ivantime000@gmail.com>
2025-03-14 11:36:48 +0700

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,7 +46,7 @@ export async function generateMetadata() {
     },
     icons: {
       icon: "/favicon.ico", // Standard favicon
-      apple: "/apple-touch-icon.png", // Apple icon (180x180 PNG)
+      apple: "/apple-icon-180x180.png", // Apple icon (180x180 PNG)
       shortcut: "/favicon.ico",
     },
   };
@@ -59,7 +59,51 @@ export default function RootLayout({ children }) {
         {/* Favicons */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/tandh.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
 
         {/* Mask Icon for Safari */}
         <link rel="mask-icon" href="/tandh.svg" color="#000000" />
